9e9427ee6d3296a00d722af4562d1b9199929dae
Eric Song <ericksong91@gmail.com>
2022-07-20 15:47:22 -0400

    commit tree 367eabf73798c0f7839fee6b8d7188d7fe881959

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+console.log(saturdayFun())
+console.log(saturdayFun("go bowling"))
+
+function mondayWork(monday = "go to the office"){
+    return `This Monday, I will ${monday}.`
+}
+
+console.log(mondayWork())
+console.log(mondayWork("go bowling"))
+
+function wrapAdjective(emphasis = "*"){
+    return function (phrase = "special") {
+        return `You are ${emphasis}${phrase}${emphasis}!`
+    };
+
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"))
